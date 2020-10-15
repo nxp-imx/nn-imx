f087b64b6f4c16943954c5c54f0dc3cd10ff80c5
Xianzhong <xianzhong.li@nxp.com>
2020-10-15 22:27:01 +0800

    update yocto build toolchain with 5.4-zeus
    Signed-off-by: Xianzhong <xianzhong.li@nxp.com>

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 echo "Please set your compile toolchain first"
 export AQROOT=`pwd`
-. /opt/fsl-imx-internal-xwayland/4.19-warrior/environment-setup-aarch64-poky-linux
-make install
+export CC="ccache $CC"
+export CXX="ccache $CXX"
+export CPP="ccache $CPP"
+. /opt/fsl-imx-internal-xwayland/5.4-zeus/environment-setup-aarch64-poky-linux
+make install -j8
