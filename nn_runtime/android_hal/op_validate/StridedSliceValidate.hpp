ead0e4d4797ee53628f45eb7c4b3d2994813e5ad
Juku.Jia <Juku.Jia@verisilicon.com>
2020-11-12 08:32:26 +0000

    MA-18240-1 [#imx-2267] [PATCH 2/4] [NNRT-702]:Reject StridedSlice that split on batch
    Driver do not support split on batch
    
    fix case:TestRandomGraph/SingleOperationTest.STRIDED_SLICE_V1_1/35
    
    Change-Id: Ibe0c94ff8ac457702ed5895c3e33f3c214b72958
    Signed-off-by: Juku.Jia <Juku.Jia@verisilicon.com>
    Reviewed-on: http://192.168.32.113/review/8601
    Tested-by: Jia <juku.jia@verisilicon.com>
    Reviewed-by: xiang.zhang <Xiang.Zhang@verisilicon.com>
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- nn_runtime/android_hal/op_validate/StridedSliceValidate.hpp
+++ nn_runtime/android_hal/op_validate/StridedSliceValidate.hpp
@@ -36,10 +36,52 @@ class StridedSliceValidate : public OperationValidate<T_model, T_Operation> {
     StridedSliceValidate(const T_model& model, const T_Operation& operation)
         : OperationValidate<T_model, T_Operation>(model, operation){};
 
+    bool IsSplitOnBatch(std::string& reason) {
+        bool isSupport = true;
+        {
+            const uint32_t input = 0;
+            const uint32_t operand_begin = 1;
+            const uint32_t operand_end = 2;
+            const uint32_t operand_stride = 3;
+
+            auto input_rank =
+                this->m_Model.operands[this->m_Operation.inputs[input]].dimensions.size();
+            auto input_batch =
+                this->m_Model.operands[this->m_Operation.inputs[input]].dimensions[0];
+
+            struct vsi_driver::VsiRTInfo rt;
+
+            auto begin_vec = reinterpret_cast<const int32_t*>(get_buffer::getOperandDataPtr(
+                this->m_Model,
+                this->m_Model.operands[this->m_Operation.inputs[operand_begin]],
+                rt));
+            auto end_vec = reinterpret_cast<const int32_t*>(get_buffer::getOperandDataPtr(
+                this->m_Model, this->m_Model.operands[this->m_Operation.inputs[operand_end]], rt));
+            auto stride_vec = reinterpret_cast<const int32_t*>(get_buffer::getOperandDataPtr(
+                this->m_Model,
+                this->m_Model.operands[this->m_Operation.inputs[operand_stride]],
+                rt));
+
+            bool isSplitNotOnBatch =
+                (begin_vec[0] == 0) && (end_vec[0] == input_batch) && (stride_vec[0] == 1);
+            LOG(INFO) << "begin[o] = " << begin_vec[0] << ", end[0] = " << end_vec[0]
+                      << ", input_rank = " << input_rank << ", Input_Batch = " << input_batch
+                      << ", stride[0] = " << stride_vec[0];
+
+            if (input_rank == 4 && !isSplitNotOnBatch) {
+                reason +=
+                    "Reject StrideSlice because split on Batch which we don't handled it with SW";
+                isSupport &= false;
+            }
+        }
+
+        return isSupport;
+    }
+
     bool SignatureCheck(std::string& reason) override {
         if (::hal::limitation::nnapi::match("StridedSlice_Inputs", this->InputArgTypes()) &&
             ::hal::limitation::nnapi::match("StridedSlice_Outputs", this->OutputArgTypes())) {
-            bool is_support = true;
+            bool is_support = IsSplitOnBatch(reason);
             auto operation = this->OperationForRead();
             for (auto input_idx = 1; input_idx < operation.inputs.size(); ++input_idx) {
                 if (!this->IsConstantTensor(operation.inputs[input_idx])) {
