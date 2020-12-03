82a164eff173dcd8188dc6ebc06cbac1584bff0a
Zongwu.Yang <Zongwu.Yang@verisilicon.com>
2020-12-03 11:11:18 +0800

    Fix permute issue on shared constant operand
    Change-Id: I97ee390dfd267bd83703286f5f085b7d918ae5ea
    Signed-off-by: Zongwu.Yang <Zongwu.Yang@verisilicon.com>

--- nn_runtime/nnrt/op/operand.hpp
+++ nn_runtime/nnrt/op/operand.hpp
@@ -71,7 +71,15 @@ using OperandPtr = std::shared_ptr<Operand>;
 
 class Operand : public BaseOperand {
    public:
-    void setPerm(const std::vector<uint32_t>& perm) { perm_ = perm; }
+    void setPerm(const std::vector<uint32_t>& perm) {
+        /* Shared constant operand may be broadcasted to different shapes, such as 1x1 or 1, so the
+         * permute on this shared constant operand may has the issue, the perm vector is modified
+         * two times. So we do not neet to modify the perm vector, when the perm vector has been
+         * setted. Because the share operand must has the same layout for its consumer nodes.*/
+        if (perm_.empty()) {
+            perm_ = perm;
+        }
+    }
 
     std::vector<uint32_t>& perm() { return perm_; };
 
