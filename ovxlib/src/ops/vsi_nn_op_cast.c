5505c4d0febafe642e102bb882d1737cfa0008b6
yao.liu <Yao.Liu@verisilicon.com>
2020-12-10 14:47:50 +0800

    fix the bug of op check
    Change-Id: I88d9b6ba397674bcd06763e32c2e9bd1cc186590

--- ovxlib/src/ops/vsi_nn_op_cast.c
+++ ovxlib/src/ops/vsi_nn_op_cast.c
@@ -167,6 +167,13 @@ static vsi_bool op_check
         IO_TYPE(D_F16,  D_I8)
         IO_TYPE(D_F16,  D_F16)
         IO_TYPE(D_BOOL8,  D_F16)
+        IO_TYPE(D_BOOL8,  D_BOOL8)
+        IO_TYPE(D_U8|Q_ASYM,  D_U8|Q_ASYM)
+        IO_TYPE(D_I8|Q_DFP,   D_I8|Q_DFP)
+        IO_TYPE(D_I16|Q_DFP,  D_I16|Q_DFP)
+        IO_TYPE(D_U8|Q_ASYM,  D_F32)
+        IO_TYPE(D_U8|Q_ASYM,  D_I32)
+        IO_TYPE(D_BF16,   D_BF16)
     END_IO_TYPE_DECL(CAST)
     if(!VALIDATE_OP_IO_TYPES(CAST, self, inputs, self->input.num, outputs, self->output.num)) {
         char* desc = generate_op_io_types_desc(inputs,
