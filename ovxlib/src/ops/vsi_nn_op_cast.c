09ead55996e5e13db21df347826743eada0e76e8
Jiang Bo <bo.jiang@verisilicon.com>
2021-01-06 12:56:31 +0800

    Add supported type for cast
    Issue: 28998
    
    Change-Id: I10c259b422c31a5149575220125a8761ec50dfbb
    Signed-off-by: Jiang Bo <bo.jiang@verisilicon.com>

--- ovxlib/src/ops/vsi_nn_op_cast.c
+++ ovxlib/src/ops/vsi_nn_op_cast.c
@@ -176,6 +176,7 @@ static vsi_bool op_check
         IO_TYPE(D_U8|Q_ASYM,  D_F32)
         IO_TYPE(D_U8|Q_ASYM,  D_I32)
         IO_TYPE(D_BF16,   D_BF16)
+        IO_TYPE(D_U8,   D_F16)
     END_IO_TYPE_DECL(CAST)
     if(!VALIDATE_OP_IO_TYPES(CAST, self, inputs, self->input.num, outputs, self->output.num)) {
         char* desc = generate_op_io_types_desc(inputs,
