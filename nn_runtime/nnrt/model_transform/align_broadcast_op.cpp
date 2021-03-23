475f43b0625d8ded20daeb12353729e412a39262
xiang.zhang <xiang.zhang@verisilicon.com>
2021-03-23 13:32:32 +0800

    Revert "[NNRT-978] fix AlignBroadcastOp for lstm model"
    This reverts commit c8d44117fb5cd649b1802e5c9c11bfc3093f93a8.
    
    Change-Id: I03e46854f2233f81dbd30ba5a38b40b71d9f7170

--- nn_runtime/nnrt/model_transform/align_broadcast_op.cpp
+++ nn_runtime/nnrt/model_transform/align_broadcast_op.cpp
@@ -82,12 +82,6 @@ namespace nnrt
         OperandPtr operand = inputs[small_input_idx];
 
         if (operand->isConst()) {
-            int32_t kernelIndex;
-            auto kernelOperand = model->cloneOperand(operand, &kernelIndex);
-            model->setOperandValue(
-                kernelIndex, operand->weak_mem_ref.lock()->address_, operand->bytes());
-            operation->replaceInputs(operation->inputs()[small_input_idx], kernelIndex);
-            operand = kernelOperand;
             for (int32_t i = 0; i < dim; ++i) {
                 operand->dimensions.insert(operand->dimensions.begin(), 1);
             }
