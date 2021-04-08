c1f43b4f5af133a01e0f741e2913623fe1561c41
Shenglin.Xu <Shenglin.Xu@verisilicon.com>
2021-04-08 12:07:37 +0800

    add new op into the ovxlib
    1. add group normalize
    
    issue: support group normalization operation
    Change-Id: I7dd880bd3421f309757867a9de1126dea320bc4e

--- ovxlib/include/vsi_nn_node_type.h
+++ ovxlib/include/vsi_nn_node_type.h
@@ -164,6 +164,7 @@
 #include "ops/vsi_nn_op_resize_1d_bilinear_internal.h"
 #include "ops/vsi_nn_op_resize_1d_nearest_internal.h"
 #include "ops/vsi_nn_op_upsamplescale.h"
+#include "ops/vsi_nn_op_groupnormalize.h"
 /* custom node head define define */
 #include "custom/vsi_nn_custom_node_type.h"
 
@@ -314,6 +315,7 @@ typedef union _vsi_nn_nn_param
     vsi_nn_resize_1d_bilinear_internal_param resize_1d_bilinear_internal;
     vsi_nn_resize_1d_nearest_internal_param resize_1d_nearest_internal;
     vsi_nn_upsamplescale_param      upsamplescale;
+    vsi_nn_groupnormalize_param     groupnorm;
     uint8_t                         client_param[128];
 
     /* custom node data struct define */
