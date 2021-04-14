4e4c3889196fb0a52fb192d2018b436f7b7bca96
xiang.zhang <xiang.zhang@verisilicon.com>
2021-04-14 22:17:14 +0800

    Map pRelu as LeakyRelu if ratio is same cross channel
    Change-Id: I816036340064dc0057f49b0305f5709789b47ecd
    Signed-off-by: xiang.zhang <xiang.zhang@verisilicon.com>

--- nn_runtime/nnrt/op/operand.hpp
+++ nn_runtime/nnrt/op/operand.hpp
@@ -55,6 +55,7 @@ struct BaseOperand {
     QuantizationParams quant;
     union {
         bool boolean;
+        uint8_t u8;
         int32_t int32;
         uint32_t uint32;
         uint16_t float16;
