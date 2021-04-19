538445ae9f191524bfc3555e33fc9d6f303e4782
Xia Kaihong <kaihong.xia@verisilicon.com>
2021-04-19 10:45:53 +0800

    add ceil operation
    Change-Id: Ibada10ab3f977c70f2810b913a7c0ded8ee397c3

--- ovxlib/src/utils/vsi_nn_code_generator.c
+++ ovxlib/src/utils/vsi_nn_code_generator.c
@@ -421,6 +421,7 @@ static _op_param_gen_t s_op_gen[] =
     /* UPSAMPLESCALE */         NULL,
     /* GROUPNORM */             NULL,
     /* ROUND */                 NULL,
+    /* CEIL */                  NULL,
 };
 _compiler_assert( _cnt_of_array(s_op_gen) == VSI_NN_OP_NUM, vsi_nn_code_generator_c );
 
