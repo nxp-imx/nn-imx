ab3abcb22f094a8bc2ef696ad65010ebb3ab39a0
Xia Kaihong <kaihong.xia@verisilicon.com>
2021-04-21 16:33:31 +0800

    add supported type for svdf and instance norm ops
    Change-Id: I67b32b0ab123aeea78424a8be035f6a659ad49b7

--- ovxlib/src/ops/vsi_nn_op_instancenormalize.c
+++ ovxlib/src/ops/vsi_nn_op_instancenormalize.c
@@ -211,7 +211,9 @@ static vsi_bool op_check
 {
     BEGIN_IO_TYPE_DECL(INSTANCE_NORM, 3, 1)
         IO_TYPE(D_F16,  D_F32,  D_F16,  D_F16)
+        IO_TYPE(D_F16,  D_F16,  D_F16,  D_F16)
         IO_TYPE(D_F32,  D_F32,  D_F16,  D_F32)
+        IO_TYPE(D_F32,  D_F16,  D_F16,  D_F32)
         IO_TYPE(D_F32,  D_F32,  D_F32,  D_F32)
         IO_TYPE(D_I32,  D_F32,  D_F16,  D_I32)
         IO_TYPE(D_I32,  D_F32,  D_F16,  D_F32)
