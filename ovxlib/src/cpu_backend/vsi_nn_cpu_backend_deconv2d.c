ae16ad43f3233ec232e1d624de26e3c12fc2a3e2
Xia Kaihong <kaihong.xia@verisilicon.com>
2021-05-11 15:09:45 +0800

    mov some ops sw implementation to kernel/cpu folder
    cpu backend conv2d/deconv2d, bbox transform and proposal to kernel folder
    
    Change-Id: Ibebe243ed33f84bf5aa77597670ec48daa722691

--- ovxlib/src/cpu_backend/vsi_nn_cpu_backend_deconv2d.c
+++ ovxlib/src/cpu_backend/vsi_nn_cpu_backend_deconv2d.c
@@ -26,168 +26,16 @@
 
 #include "vsi_nn_log.h"
 #include "client/vsi_nn_vxkernel.h"
+#include "kernel/vsi_nn_kernel.h"
 #include "ops/vsi_nn_op_deconvolution.h"
 
-//#define _ARG_NUM            (9)
-#define _ARG_NUM            (6)
 #define _INPUT_NUM          (3)
 #define _OUTPUT_NUM         (1)
 #define _VX_KERNEL_VAR      (vx_cpu_backend_kernel_DECONV2D)
 #define _IO_NUM             (_INPUT_NUM + _OUTPUT_NUM)
-#define _PARAM_NUM          (_ARG_NUM + _IO_NUM)
-
-extern vx_kernel_description_t * vx_kernel_DECONV2D_list[];
 
 extern vsi_nn_op_proc_t vsi_nn_op_DECONVOLUTION;
 #define InternalDeconv2DOp vsi_nn_op_DECONVOLUTION
-
-static void _set_inputs_outputs
-    (
-    vx_reference * params,
-    vsi_nn_tensor_t ** inputs,
-    vsi_nn_tensor_t ** outputs
-    )
-{
-    uint32_t i;
-    uint32_t cnt;
-
-    /* Set inputs */
-    cnt = 0;
-    for( i = 0; i < _INPUT_NUM; i ++, cnt ++ )
-    {
-        params[cnt] = inputs[i] ? (vx_reference)inputs[i]->t : NULL;
-    }
-
-    /* Set outputs */
-    for( i = 0; i < _OUTPUT_NUM; i ++, cnt ++ )
-    {
-        params[cnt] = (vx_reference)outputs[i]->t;
-    }
-} /* _set_inputs_outputs() */
-
-static vsi_status _create_params
-    (
-    vsi_nn_node_t * node,
-    vx_reference * params,
-    uint32_t num
-    )
-{
-    vsi_status status;
-    vx_context ctx;
-    vsi_nn_deconv_param * p;
-    if( 0 == num )
-    {
-        return VSI_SUCCESS;
-    }
-    memset( params, 0, sizeof( vx_reference * ) * num );
-    p = &node->nn_param.deconv;
-    ctx = vxGetContext( (vx_reference)node->graph->g );
-    /* Init parameters */
-    #define _SET_PARAM( i, type, arg ) do{ \
-        params[i] = (vx_reference)vxCreateScalar( ctx, type, &p->arg ); \
-        status = vxGetStatus( params[i] ); \
-        if( VSI_SUCCESS != status ) { \
-            goto set_param_error; \
-            } \
-        } while(0)
-    _SET_PARAM( 0, VSI_NN_TYPE_INT32, stride[0] );
-    _SET_PARAM( 1, VSI_NN_TYPE_INT32, stride[1] );
-    _SET_PARAM( 2, VSI_NN_TYPE_INT32, pad[0] );
-    _SET_PARAM( 3, VSI_NN_TYPE_INT32, pad[1] );
-    _SET_PARAM( 4, VSI_NN_TYPE_INT32, pad[2] );
-    _SET_PARAM( 5, VSI_NN_TYPE_INT32, pad[3] );
-    //_SET_PARAM( 6, VSI_NN_TYPE_INT32, dilation[0] );
-    //_SET_PARAM( 7, VSI_NN_TYPE_INT32, dilation[1] );
-    //_SET_PARAM( 8, VSI_NN_TYPE_INT32, multiplier );
-    #undef _SET_PARAM
-set_param_error:
-
-    return status;
-} /* _create_params */
-
-static void _release_params
-    (
-    vx_reference * params,
-    uint32_t num
-    )
-{
-    uint32_t i;
-    vx_scalar scalar;
-    for( i = 0; i < num; i ++ )
-    {
-        scalar = (vx_scalar)params[i];
-        vxReleaseScalar( &scalar );
-    }
-} /* _release_params() */
-
-static vsi_status cpu_op_compute
-    (
-    vsi_nn_node_t * self,
-    vsi_nn_tensor_t ** inputs,
-    vsi_nn_tensor_t ** outputs
-    )
-{
-    vsi_status status = VSI_SUCCESS;
-    vx_reference params[_PARAM_NUM];
-    vx_reference * args;
-
-    args = &params[_IO_NUM];
-
-    if( NULL == self->n )
-    {
-        return VSI_FAILURE;
-    }
-
-    /* Set inputs and outputs */
-    _set_inputs_outputs( params, inputs, outputs );
-
-    /* Init parameters. */
-    _create_params( self, args, _ARG_NUM );
-
-    /* Pass parameters to node. */
-    status = vsi_nn_ClientNodePassParameters( self->n, params, _PARAM_NUM );
-
-    _release_params( args, _ARG_NUM );
-
-    return status;
-}
-
-static vsi_nn_op_compute_t op_compute_list[] =
-{
-    cpu_op_compute,
-    NULL
-};
-
-static vsi_status client_op_compute
-    (
-    vsi_nn_node_t * self,
-    vsi_nn_tensor_t ** inputs,
-    vsi_nn_tensor_t ** outputs
-    )
-{
-    vsi_status status;
-    vsi_nn_kernel_info_t kernel_info;
-
-    memset(&kernel_info, 0x0, sizeof(vsi_nn_kernel_info_t));
-    status = VSI_FAILURE;
-    kernel_info.type = VX_KERNEL_TYPE_CPU;
-    kernel_info.kernel = vx_kernel_DECONV2D_list;
-
-    kernel_info.kernel_index = 0;
-    kernel_info.init_index = 0;
-
-    self->n = vsi_nn_RegisterClientKernelAndNewNode(
-            self->graph, &kernel_info);
-    if( NULL == self->n )
-    {
-        return VSI_FAILURE;
-    }
-    if (NULL != op_compute_list[kernel_info.init_index])
-    {
-        status = op_compute_list[kernel_info.init_index](self, inputs, outputs);
-    }
-    return status;
-} /* client_op_compute() */
 
 static vsi_status op_compute
     (
@@ -196,17 +44,32 @@ static vsi_status op_compute
     vsi_nn_tensor_t ** outputs
     )
 {
-    vsi_status status;
-    if( inputs[0]->attr.dtype.vx_type == VSI_NN_TYPE_UINT8
+    vsi_status status = VSI_FAILURE;
+    vsi_nn_kernel_param_t * param = NULL;
+
+    param = vsi_nn_kernel_param_create();
+
+    vsi_nn_kernel_param_add_buffer( param, "stride", self->nn_param.deconv.stride, 2 );
+    vsi_nn_kernel_param_add_buffer( param, "pad", self->nn_param.deconv.pad, 4 );
+
+    if ( inputs[0]->attr.dtype.vx_type == VSI_NN_TYPE_UINT8
             && outputs[0]->attr.dtype.vx_type == VSI_NN_TYPE_UINT8
             && self->nn_param.deconv.group <= 1)
     {
-        status = client_op_compute( self, inputs, outputs );
+        self->n = (vx_node)vsi_nn_kernel_selector( self->graph, "cpu beckend conv2d",
+                inputs, 3, outputs, 1, param );
+
+        if ( self->n )
+        {
+            status = VSI_SUCCESS;
+        }
     }
     else
     {
-        status = InternalDeconv2DOp.compute(self, inputs, outputs);
+        status = InternalDeconv2DOp.compute( self, inputs, outputs );
     }
+
+    vsi_nn_kernel_param_release( &param );
     return status;
 } /* op_compute() */
 
