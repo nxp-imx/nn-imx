b1be7761cceeee91ff8ad1b70255076a1e690d3b
Xia Zhao <Zhao.Xia@verisilicon.com>
2021-05-26 11:24:47 +0800

    Add kernel param const buffer type
    Change-Id: I9bbb41780ca4a5ee0937be9d1cc4ffd268f2363b
    Signed-off-by: Xia Zhao <Zhao.Xia@verisilicon.com>

--- ovxlib/src/kernel/vsi_nn_kernel_param.c
+++ ovxlib/src/kernel/vsi_nn_kernel_param.c
@@ -42,6 +42,7 @@ typedef enum
     _PARAM_I64,
     _PARAM_F32,
     _PARAM_BUFFER,
+    _PARAM_CONST_BUFFER,
     _PARAM_STR,
 } _param_dtype_e;
 
@@ -178,12 +179,12 @@ vsi_bool vsi_nn_kernel_param_add_const_buffer
     CHECK_PARAM_NULL( key, FALSE, "Param key is null ptr." );
     p = malloc( sizeof(_param_type) );
     CHECK_PARAM_NULL( p, FALSE, "Out of memory, add param fail." );
-    p->type = _PARAM_BUFFER;
+    p->type = _PARAM_CONST_BUFFER;
     p->value.const_buffer = value;
     p->size = size;
     vsi_nn_hashmap_add( params, key, p );
     return TRUE;
-} /* vsi_nn_kernel_param_add_buffer() */
+} /* vsi_nn_kernel_param_add_const_buffer() */
 
 const void* vsi_nn_kernel_param_get_const_buffer
     ( const vsi_nn_kernel_param_t * params, const char * key, size_t * size)
@@ -193,9 +194,9 @@ const void* vsi_nn_kernel_param_get_const_buffer
     CHECK_PARAM_NULL( key, FALSE, "Param key is null ptr." );
     p = vsi_nn_hashmap_get( params, key );
     CHECK_PARAM_NULL( p, 0, "Key %s not in params.", key );
-    if( p->type != _PARAM_BUFFER )
+    if( p->type != _PARAM_CONST_BUFFER )
     {
-        VSILOGW("Key %s is not \"buffer\"", key );
+        VSILOGW("Key %s is not \"const buffer\"", key );
     }
     if( size != NULL )
     {
