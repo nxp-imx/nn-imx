4c5de51f48d7edab52f7b4bdd36f6d83e74dec20
Xia Kaihong <kaihong.xia@verisilicon.com>
2021-06-07 18:09:19 +0800

    add NonMaxSuppression operation
    Implements (Single-Class) Soft NMS (with Gaussian weighting).
    Supports functionality of TensorFlow ops NonMaxSuppressionV4 & V5.
    Reference: "Soft-NMS - Improving Object Detection With One Line of Code"
               [Bodla et al, https://arxiv.org/abs/1704.04503]
    Implementation adapted from the TensorFlow NMS code at
    tensorflow/core/kernels/non_max_suppression_op.cc.
    
    Arguments:
     boxes: box encodings in format [y1, x1, y2, x2], shape: [num_boxes, 4]
     num_boxes: number of candidates
     scores: scores for candidate boxes, in the same order. shape: [num_boxes]
     max_output_size: the maximum number of selections.
     iou_threshold: Intersection-over-Union (IoU) threshold for NMS
     score_threshold: All candidate scores below this value are rejected
     soft_nms_sigma: Soft NMS parameter, used for decaying scores
    
    Outputs:
     selected_indices: all the selected indices. Underlying array must have
       length >= max_output_size. Cannot be null.
     selected_scores: scores of selected indices. Defer from original value for
       Soft NMS. If not null, array must have length >= max_output_size.
     num_selected_indices: Number of selections. Only these many elements are
       set in selected_indices, selected_scores. Cannot be null.
    
    Change-Id: If2d49062ea8b30d870f28b15292bd5b4888d5c8b

--- ovxlib/include/vsi_nn_node_type.h
+++ ovxlib/include/vsi_nn_node_type.h
@@ -168,6 +168,7 @@
 #include "ops/vsi_nn_op_sequence_mask.h"
 #include "ops/vsi_nn_op_repeat.h"
 #include "ops/vsi_nn_op_one_hot.h"
+#include "ops/vsi_nn_op_nms.h"
 /* custom node head define define */
 #include "custom/vsi_nn_custom_node_type.h"
 
@@ -322,6 +323,7 @@ typedef union _vsi_nn_nn_param
     vsi_nn_sequence_mask_param      sequence_mask;
     vsi_nn_repeat_param             repeat;
     vsi_nn_one_hot_param            one_hot;
+    vsi_nn_nms_param                nms;
     uint8_t                         client_param[128];
 
     /* custom node data struct define */
