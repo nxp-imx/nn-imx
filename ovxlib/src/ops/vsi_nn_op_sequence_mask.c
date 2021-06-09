77078521cec1c117af5d46e9fb2d93bc10bd514d
Jiang Bo <bo.jiang@verisilicon.com>
2021-06-09 22:31:18 +0800

    Add support types for sequence_mask
    Change-Id: I9af17ce9e58b47de87ff0c033606fd771ad36837
    Signed-off-by: Jiang Bo <bo.jiang@verisilicon.com>

--- ovxlib/src/ops/vsi_nn_op_sequence_mask.c
+++ ovxlib/src/ops/vsi_nn_op_sequence_mask.c
@@ -98,6 +98,10 @@ static vsi_bool op_check
         IO_TYPE(D_I32,  D_I32, D_I32)
         IO_TYPE(D_I32,  D_I32, D_I32|Q_DFP)
         IO_TYPE(D_I32,  D_I32, D_F32)
+
+        IO_TYPE(D_U8|Q_ASYM,  D_NONE, D_U8|Q_ASYM)
+        IO_TYPE(D_I32,  D_NONE, D_U8|Q_ASYM)
+        IO_TYPE(D_I32,  D_NONE, D_BOOL8)
     END_IO_TYPE_DECL(SEQUENCE_MASK)
     if (!VALIDATE_OP_IO_TYPES(SEQUENCE_MASK, self, inputs, self->input.num, outputs, self->output.num)) {
         char* desc = generate_op_io_types_desc(inputs,
