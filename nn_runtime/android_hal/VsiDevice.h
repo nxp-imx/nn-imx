a8474573d0060d897c4a702d4957c7001a8e0026
xiang.zhang <xiang.zhang@verisilicon.com>
2021-06-15 16:20:51 +0800

    Fix build error on Android 11
    Change-Id: I651fd42734589a80ea5d454f934ed3d1185306e0
    Signed-off-by: xiang.zhang <xiang.zhang@verisilicon.com>
    Reviewed-on: http://192.168.32.113/review/11167
    Reviewed-by: Xiang Zhang <Xiang.Zhang@verisilicon.com>
    Tested-by: Xiang Zhang <Xiang.Zhang@verisilicon.com>

--- nn_runtime/android_hal/VsiDevice.h
+++ nn_runtime/android_hal/VsiDevice.h
@@ -110,7 +110,7 @@ class VsiDevice : public HalPlatform::Device
             const V1_3::Model& model, ExecutionPreference preference,
             Priority priority, const OptionalTimePoint& deadline,
             const hidl_vec<hidl_handle>& modelCache,
-            const hidl_vec<hidl_handle>& dataCache, const HalCacheToken& token,
+            const hidl_vec<hidl_handle>& dataCache, const CacheToken& token,
             const sp<V1_3::IPreparedModelCallback>& callback);
 
     Return<V1_3::ErrorStatus> prepareModelFromCache_1_3(
