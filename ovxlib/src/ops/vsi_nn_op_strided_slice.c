8d05daca13bc8e83a370727b490f8e0a7a1f804c
Jiang Bo <bo.jiang@verisilicon.com>
2021-09-16 14:52:46 +0800

    Add BOOL8 support for stridedslice
    Change-Id: I3193047aeff42f3ff318f96284ffbd1b3b34c992
    Signed-off-by: Jiang Bo <bo.jiang@verisilicon.com>

--- ovxlib/src/ops/vsi_nn_op_strided_slice.c
+++ ovxlib/src/ops/vsi_nn_op_strided_slice.c
@@ -449,6 +449,7 @@ static vsi_bool op_check
         IO_TYPE(D_F32,          D_BF16)
         IO_TYPE(D_BF16,         D_F32)
         IO_TYPE(D_I32,          D_I32)
+        IO_TYPE(D_BOOL8,        D_BOOL8)
 
         /* HW 9.0.1 */
         IO_TYPE(D_U8|Q_ASYM,    D_I8|Q_DFP)
