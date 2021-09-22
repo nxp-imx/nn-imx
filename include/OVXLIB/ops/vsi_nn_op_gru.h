4d45d90e512fc29d1c8c19add22ebe869da56633
Eric Guo <eric.guo@nxp.com>
2021-09-22 15:16:55 +0800

    MGS-6395 [#ccc] Add OVXLIB new headers for patch_6.4.3.p2_20210917
    Signed-off-by: Eric Guo <eric.guo@nxp.com>

--- include/OVXLIB/ops/vsi_nn_op_gru.h
+++ include/OVXLIB/ops/vsi_nn_op_gru.h
@@ -21,64 +21,58 @@
 *    DEALINGS IN THE SOFTWARE.
 *
 *****************************************************************************/
-#ifndef _VSI_NN_OP_PRE_PROCESS_H
-#define _VSI_NN_OP_PRE_PROCESS_H
+#ifndef _VSI_NN_OP_GRU_H
+#define _VSI_NN_OP_GRU_H
 
 #include "vsi_nn_types.h"
-#include "vsi_nn_pre_post_process.h"
 
-typedef  vsi_nn_preprocess_source_format_e vsi_nn_pre_process_type_e;
-
+/* Define the inputs and outputs for GRU Layer */
 enum
 {
-    PRE_PROCESS_INPUT0 = 0,
+    GRU_IN_INPUT        = 0,
+    GRU_IN_H_STATE      = 1,
 
-    PRE_PROCESS_INPUT1,
-    PRE_PROCESS_INPUT2,
+    /* input kernel */
+    GRU_IN_KERNEL_I2Z   = 2,
+    GRU_IN_KERNEL_I2R   = 3,
+    GRU_IN_KERNEL_I2H   = 4,
 
-    PRE_PROCESS_INPUT_CNT,
+    /* recurrent kernel */
+    GRU_IN_KERNEL_R2Z   = 5,
+    GRU_IN_KERNEL_R2R   = 6,
+    GRU_IN_KERNEL_R2H   = 7,
 
-    PRE_PROCESS_OUTPUT = 0,
+    /* input bias */
+    GRU_IN_BIAS_I2Z     = 8,
+    GRU_IN_BIAS_I2R     = 9,
+    GRU_IN_BIAS_I2H     = 10,
 
-    PRE_PROCESS_OUTPUT_CNT
+    /* recurrent bias */
+    GRU_IN_BIAS_R2Z     = 11,
+    GRU_IN_BIAS_R2R     = 12,
+    GRU_IN_BIAS_R2H     = 13,
+
+    GRU_IN_CNT,
+
+    GRU_OUT_OUTPUT      = 0,
+    GRU_OUT_H_STATE     = 1,
+
+    GRU_OUT_CNT
 };
 
-#define _VSI_NN_PRE_PROCESS_LOCAL_TENSOR_NUM 10
-typedef struct _vsi_nn_pre_process_lcl_data
+typedef struct _vsi_nn_gru_param
 {
-    vsi_nn_tensor_t *local_tensor[_VSI_NN_PRE_PROCESS_LOCAL_TENSOR_NUM];
-} vsi_nn_pre_process_lcl_data;
+    struct _vsi_nn_gru_local * local;
 
-typedef struct _vsi_nn_pre_process_param
-{
-    struct
-    {
-        uint32_t left;
-        uint32_t top;
-        uint32_t width;
-        uint32_t height;
-    } rect;
+    uint32_t num_units;
+    vsi_nn_activation_e activation;
+    vsi_nn_activation_e recurrent_activation;
+    vsi_bool reset_after;
+    vsi_bool return_sequences;
+    vsi_bool time_major;
+} vsi_nn_gru_param;
+_compiler_assert(offsetof(vsi_nn_gru_param, local) == 0, \
+                 vsi_nn_gru_h );
 
-    struct
-    {
-        vsi_size_t   *size;
-        uint32_t   dim_num;
-    } output_attr;
-
-    uint32_t * perm;
-    uint32_t   dim_num;
-
-    struct
-    {
-        float   mean[3];
-        float   scale;
-    } norm;
-
-    vsi_bool reverse_channel;
-
-    vsi_nn_pre_process_type_e type;
-
-    vsi_nn_pre_process_lcl_data *local;
-} vsi_nn_pre_process_param;
 #endif
 
