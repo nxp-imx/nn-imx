c81b78cdef553d2adf2ce0a23abbcdafc0cb111e
Xia Kaihong <kaihong.xia@verisilicon.com>
2022-09-15 13:56:57 +0800

    add sign operation sp kernel
    2 instructions per output pixel
    
    Change-Id: I6272f697663535104fe172c70a8cf57e13e819c7

--- ovxlib/src/kernel/sp/eltwise_unary_sp.c
+++ ovxlib/src/kernel/sp/eltwise_unary_sp.c
@@ -659,6 +659,85 @@ final:
     return (vsi_nn_kernel_node_t)node;
 }
 
+vsi_nn_kernel_node_t vsi_nn_sp_sign_node
+    (
+        vsi_nn_graph_t              * graph,
+        vsi_nn_tensor_t             * input,
+        vsi_nn_tensor_t             * output
+    )
+{
+    const int32_t spInitInstsNum = 1;
+    const int32_t spLoopInstsNum = 2;
+    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
+
+    const uint32_t input_count = 1;
+    const uint32_t output_count = 1;
+    vx_tensor inputs_tensor[1] = {NULL};
+    vx_tensor outputs_tensor[1] = {NULL};
+    vx_node node = NULL;
+
+    vsi_nn_spinst_t *spinst = NULL;
+    vsi_nn_spinst_inst_param sp_insts_param[3];
+    vsi_nn_spinst_attr_t attr;
+
+    float output_scale = 1.0f / vsi_nn_get_tensor_scale(output);
+
+    vsi_status status = VSI_FAILURE;
+
+    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
+    vsi_nn_init_spinst_attr(&attr);
+
+
+    /* init inst0: r2 = 1 / output_scale */
+    status  = vsi_nn_sp_move_constant(&sp_insts_param[0], output_scale, VSI_NN_SP_SR2);
+    /* loop inst0: r1 = in + r7 | out = r8 ? r4 : r5 */
+    status  |= vsi_nn_sp_add(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR7, VSI_NN_SP_SR1);
+    status  |= vsi_nn_sp_move_sel0(&sp_insts_param[1], VSI_NN_SP_SR8, VSI_NN_SP_SR4, VSI_NN_SP_SROUT);
+    /* loop inst1: r8 = r1 * r6 | r5 = r1 ? r2 : r3 */
+    status |= vsi_nn_sp_mul(&sp_insts_param[2], VSI_NN_SP_SR1, VSI_NN_SP_SR6, VSI_NN_SP_SR8);
+    status |= vsi_nn_sp_move_sel0(&sp_insts_param[2], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SR5);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
+    attr.input_setup = VSI_NN_SP_INPUT_SETUP_SINGLE_INPUT;
+
+    attr.prog_init_instr_num = spInitInstsNum;
+    attr.prog_loop_instr_num = spLoopInstsNum;
+    attr.ignored_leading_outputs = 3;
+    attr.flush_cycle_num = 6;
+
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, 0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR4(attr, -output_scale);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR5_LOW_PRECISION(attr, 0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR6(attr, -1);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR7(attr, 0);
+
+    spinst = vsi_nn_create_spinst(graph);
+    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
+    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
+    status |= vsi_nn_set_spinst_attr(spinst, attr);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    inputs_tensor[0] = input->t;
+    outputs_tensor[0] = output->t;
+    node = vxStreamProcessorNode(
+        graph->g,
+        inputs_tensor,
+        input_count,
+        outputs_tensor,
+        output_count,
+        spinst->sp,
+        NULL);
+
+final:
+    if (spinst)
+    {
+        vsi_nn_release_spinst(&spinst);
+    }
+
+    return (vsi_nn_kernel_node_t)node;
+}
+
 #define REGISTER_ELTWISE_UNARY_STREAM_PROCESSOR_KERNEL( kernel_name )   \
     static vsi_nn_kernel_node_t _##kernel_name##setup \
         ( \
@@ -782,7 +861,19 @@ REGISTER_ELTWISE_UNARY_STREAM_PROCESSOR_KERNEL( cast )
 final:
 
     return node;
-} /* clip() */
+} /* cast() */
+
+REGISTER_ELTWISE_UNARY_STREAM_PROCESSOR_KERNEL( sign )
+{
+    vsi_nn_kernel_node_t node = NULL;
+
+    node = vsi_nn_sp_sign_node(graph, inputs[0], outputs[0]);
+    CHECK_PTR_FAIL_GOTO( node, "Create sp_sign node fail.", final );
+
+final:
+
+    return node;
+} /* sign() */
 
 #undef REGISTER_ELTWISE_UNARY_STREAM_PROCESSOR_KERNEL
 
