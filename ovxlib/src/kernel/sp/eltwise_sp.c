6f512bb0335c88d47f239e0e1b740420bcbf2f69
Xia Kaihong <kaihong.xia@verisilicon.com>
2022-10-08 10:18:57 +0800

    add select operation sp kernel
    Type: New Feature
    Change-Id: I850fc015f40ac613bca38bb98666e3488c6ec6c8

--- ovxlib/src/kernel/sp/eltwise_sp.c
+++ ovxlib/src/kernel/sp/eltwise_sp.c
@@ -77,10 +77,10 @@ vsi_nn_kernel_node_t vsi_nn_sp_add_node
 
     /* init inst0: r5 = const2 */
     status  = vsi_nn_sp_move_constant(&sp_insts_param[0], const2, VSI_NN_SP_SR5);
-    /* loop inst0: r1 = clamp(in * r3, r7, r6) || r2 = r1 + r2 */
+    /* loop inst0: r1 = clamp(in * r3, r7, r6) | r2 = r1 + r2 */
     status |= vsi_nn_sp_mul_clamp(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR1);
     status |= vsi_nn_sp_add(&sp_insts_param[1], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SR2);
-    /* loop inst1: r2 = in * r4 || out = r2 + r5 */
+    /* loop inst1: r2 = in * r4 | out = r2 + r5 */
     status |= vsi_nn_sp_mul(&sp_insts_param[2], VSI_NN_SP_SRIN, VSI_NN_SP_SR4, VSI_NN_SP_SR2);
     status |= vsi_nn_sp_add(&sp_insts_param[2], VSI_NN_SP_SR2, VSI_NN_SP_SR5, VSI_NN_SP_SROUT);
     CHECK_STATUS_FAIL_GOTO(status, final );
@@ -168,10 +168,10 @@ vsi_nn_kernel_node_t vsi_nn_sp_sub_node
     /* output = in * const0 - (in1 * const1 + const2) */
     /* init inst0: r5 = const2 */
     status  = vsi_nn_sp_move_constant(&sp_insts_param[0], const2, VSI_NN_SP_SR5);
-    /* loop inst0: r1 = clamp(in * r3, r7, r6) || r2 = r1 - r2 */
+    /* loop inst0: r1 = clamp(in * r3, r7, r6) | r2 = r1 - r2 */
     status |= vsi_nn_sp_mul_clamp(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR1);
     status |= vsi_nn_sp_sub(&sp_insts_param[1], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SR2);
-    /* loop inst1: r2 = in * r4 || out = r2 - r5 */
+    /* loop inst1: r2 = in * r4 | out = r2 - r5 */
     status |= vsi_nn_sp_mul(&sp_insts_param[2], VSI_NN_SP_SRIN, VSI_NN_SP_SR4, VSI_NN_SP_SR2);
     status |= vsi_nn_sp_sub(&sp_insts_param[2], VSI_NN_SP_SR2, VSI_NN_SP_SR5, VSI_NN_SP_SROUT);
     CHECK_STATUS_FAIL_GOTO(status, final );
@@ -259,7 +259,7 @@ vsi_nn_kernel_node_t vsi_nn_sp_mul_node
 
     /* loop inst0: v11 = clamp(in * r3, r7, r6) */
     status  = vsi_nn_sp_mul_clamp(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_VR11);
-    /* loop inst1: out = v11 * r5 || r5 = in + r4 */
+    /* loop inst1: out = v11 * r5 | r5 = in + r4 */
     status |= vsi_nn_sp_mul(&sp_insts_param[1], VSI_NN_SP_VR11, VSI_NN_SP_SR5, VSI_NN_SP_SROUT);
     status |= vsi_nn_sp_add(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR4, VSI_NN_SP_SR5);
     CHECK_STATUS_FAIL_GOTO(status, final );
@@ -364,7 +364,7 @@ vsi_nn_kernel_node_t vsi_nn_sp_div_node
     status |= vsi_nn_sp_mul(&sp_insts_param[2], VSI_NN_SP_SR5, VSI_NN_SP_SR2, VSI_NN_SP_SR8);
     status |= vsi_nn_sp_add(&sp_insts_param[2], VSI_NN_SP_SR1, VSI_NN_SP_SR8, VSI_NN_SP_SR9);
     status |= vsi_nn_sp_move(&sp_insts_param[2], VSI_NN_SP_SR10, VSI_NN_SP_SR1);
-    /* loop inst3: r5 = pwlMul * pwlMul || r2 = pwlAdd + pwlAdd || r10 = r1*/
+    /* loop inst3: r5 = pwlMul * pwlMul | r2 = pwlAdd + pwlAdd || r10 = r1*/
     status |= vsi_nn_sp_mul(&sp_insts_param[3], VSI_NN_SP_PWLMUL, VSI_NN_SP_PWLMUL, VSI_NN_SP_SR5);
     status |= vsi_nn_sp_sub(&sp_insts_param[3], VSI_NN_SP_PWLADD, VSI_NN_SP_PWLADD, VSI_NN_SP_SR2);
     status |= vsi_nn_sp_move(&sp_insts_param[3], VSI_NN_SP_SR1, VSI_NN_SP_SR10);
@@ -620,6 +620,243 @@ final:
     return (vsi_nn_kernel_node_t)node;
 }
 
+vsi_nn_kernel_node_t vsi_nn_sp_select_a_node
+    (
+        vsi_nn_graph_t              * graph,
+        vsi_nn_tensor_t             * input0,
+        vsi_nn_tensor_t             * input1,
+        vsi_nn_tensor_t             * output,
+        float                         output_scale
+    )
+{
+    const int32_t spInitInstsNum = 0;
+    const int32_t spLoopInstsNum = 2;
+    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
+
+    const uint32_t input_count = 2;
+    const uint32_t output_count = 1;
+    vx_tensor inputs_tensor[2] = {NULL};
+    vx_tensor outputs_tensor[1] = {NULL};
+    vx_node node = NULL;
+
+    vsi_nn_spinst_t *spinst = NULL;
+    vsi_nn_spinst_inst_param sp_insts_param[2];
+    vsi_nn_spinst_attr_t attr;
+
+    vsi_status status = VSI_FAILURE;
+    float input0_scale = vsi_nn_get_tensor_scale(input0);
+    float scale0 = input0_scale / output_scale;
+    float clamp_min = 0;
+    float clamp_max = 0;
+
+    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
+    vsi_nn_init_spinst_attr(&attr);
+
+    vsi_nn_get_tensor_clamp_min_max(input0, &clamp_min, &clamp_max);
+    clamp_min = clamp_min * scale0;
+    clamp_max = clamp_max * scale0;
+
+    /* loop inst0: r4 = clamp(in * r3, r7, r6) | out = r2 ? r4 : r5 */
+    status  = vsi_nn_sp_mul_clamp(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR4);
+    status |= vsi_nn_sp_move_sel0(&sp_insts_param[0], VSI_NN_SP_SR2, VSI_NN_SP_SR4, VSI_NN_SP_SROUT);
+    /* loop inst1: r2 = abs(in) */
+    status |= vsi_nn_sp_abs(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR2);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    attr.ignored_leading_outputs = 2;
+    attr.flush_cycle_num = 3;
+
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, scale0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR5_LOW_PRECISION(attr, 0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR6(attr, clamp_max);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR7(attr, clamp_min);
+
+    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
+    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
+
+    attr.prog_init_instr_num = spInitInstsNum;
+    attr.prog_loop_instr_num = spLoopInstsNum;
+
+    spinst = vsi_nn_create_spinst(graph);
+    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
+    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
+    status |= vsi_nn_set_spinst_attr(spinst, attr);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    inputs_tensor[0] = input0->t;
+    inputs_tensor[1] = input1->t;
+    outputs_tensor[0] = output->t;
+    node = vxStreamProcessorNode(
+        graph->g,
+        inputs_tensor,
+        input_count,
+        outputs_tensor,
+        output_count,
+        spinst->sp,
+        NULL);
+
+final:
+    if (spinst)
+    {
+        vsi_nn_release_spinst(&spinst);
+    }
+
+    return (vsi_nn_kernel_node_t)node;
+}
+
+vsi_nn_kernel_node_t vsi_nn_sp_select_b_node
+    (
+        vsi_nn_graph_t              * graph,
+        vsi_nn_tensor_t             * input0,
+        vsi_nn_tensor_t             * input1,
+        vsi_nn_tensor_t             * output,
+        float                         output_scale
+    )
+{
+    const int32_t spInitInstsNum = 0;
+    const int32_t spLoopInstsNum = 2;
+    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
+
+    const uint32_t input_count = 2;
+    const uint32_t output_count = 1;
+    vx_tensor inputs_tensor[2] = {NULL};
+    vx_tensor outputs_tensor[1] = {NULL};
+    vx_node node = NULL;
+
+    vsi_nn_spinst_t *spinst = NULL;
+    vsi_nn_spinst_inst_param sp_insts_param[2];
+    vsi_nn_spinst_attr_t attr;
+
+    vsi_status status = VSI_FAILURE;
+    float input0_scale = vsi_nn_get_tensor_scale(input0);
+    float scale0 = input0_scale / output_scale;
+    float clamp_min = 0;
+    float clamp_max = 0;
+
+    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
+    vsi_nn_init_spinst_attr(&attr);
+
+    vsi_nn_get_tensor_clamp_min_max(input0, &clamp_min, &clamp_max);
+    clamp_min = clamp_min * scale0;
+    clamp_max = clamp_max * scale0;
+
+    /* loop inst0: r5 = clamp(in * r3, r7, r6) | out = r2 ? r4 : r5 */
+    status  = vsi_nn_sp_mul_clamp(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR5);
+    status |= vsi_nn_sp_move_sel0(&sp_insts_param[0], VSI_NN_SP_SR2, VSI_NN_SP_SR4, VSI_NN_SP_SROUT);
+    /* loop inst1: r2 = abs(in) */
+    status |= vsi_nn_sp_abs(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR2);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    attr.ignored_leading_outputs = 2;
+    attr.flush_cycle_num = 3;
+
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, scale0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR4(attr, 0);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR6(attr, clamp_max);
+    VSI_NN_SP_ATTR_SET_CONST_TO_SR7(attr, clamp_min);
+
+    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
+    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
+
+    attr.prog_init_instr_num = spInitInstsNum;
+    attr.prog_loop_instr_num = spLoopInstsNum;
+
+    spinst = vsi_nn_create_spinst(graph);
+    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
+    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
+    status |= vsi_nn_set_spinst_attr(spinst, attr);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    inputs_tensor[0] = input0->t;
+    inputs_tensor[1] = input1->t;
+    outputs_tensor[0] = output->t;
+    node = vxStreamProcessorNode(
+        graph->g,
+        inputs_tensor,
+        input_count,
+        outputs_tensor,
+        output_count,
+        spinst->sp,
+        NULL);
+
+final:
+    if (spinst)
+    {
+        vsi_nn_release_spinst(&spinst);
+    }
+
+    return (vsi_nn_kernel_node_t)node;
+}
+
+vsi_nn_kernel_node_t vsi_nn_sp_select_add_node
+    (
+        vsi_nn_graph_t              * graph,
+        vsi_nn_tensor_t             * input0,
+        vsi_nn_tensor_t             * input1,
+        vsi_nn_tensor_t             * output
+    )
+{
+    const int32_t spInitInstsNum = 0;
+    const int32_t spLoopInstsNum = 2;
+    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
+
+    const uint32_t input_count = 2;
+    const uint32_t output_count = 1;
+    vx_tensor inputs_tensor[2] = {NULL};
+    vx_tensor outputs_tensor[1] = {NULL};
+    vx_node node = NULL;
+
+    vsi_nn_spinst_t *spinst = NULL;
+    vsi_nn_spinst_inst_param sp_insts_param[2];
+    vsi_nn_spinst_attr_t attr;
+
+    vsi_status status = VSI_FAILURE;
+
+    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
+    vsi_nn_init_spinst_attr(&attr);
+
+    /* loop inst0: r5 = clamp(in * r3, r7, r6) | r1 = in */
+    status  = vsi_nn_sp_add(&sp_insts_param[0], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SROUT);
+    status |= vsi_nn_sp_move(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR1);
+    /* loop inst1: r2 = in */
+    status |= vsi_nn_sp_move(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR2);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    attr.ignored_leading_outputs = 2;
+    attr.flush_cycle_num = 3;
+
+    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
+    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
+
+    attr.prog_init_instr_num = spInitInstsNum;
+    attr.prog_loop_instr_num = spLoopInstsNum;
+
+    spinst = vsi_nn_create_spinst(graph);
+    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
+    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
+    status |= vsi_nn_set_spinst_attr(spinst, attr);
+    CHECK_STATUS_FAIL_GOTO(status, final );
+
+    inputs_tensor[0] = input0->t;
+    inputs_tensor[1] = input1->t;
+    outputs_tensor[0] = output->t;
+    node = vxStreamProcessorNode(
+        graph->g,
+        inputs_tensor,
+        input_count,
+        outputs_tensor,
+        output_count,
+        spinst->sp,
+        NULL);
+
+final:
+    if (spinst)
+    {
+        vsi_nn_release_spinst(&spinst);
+    }
+
+    return (vsi_nn_kernel_node_t)node;
+}
 
 vsi_bool vsi_nn_sp_nn_alu_support_types
     (
@@ -819,6 +1056,42 @@ final:
 
     return node;
 } /* div() */
+
+REGISTER_ELTWISE_STREAM_PROCESSOR_KERNEL( select )
+{
+    vsi_nn_kernel_node_t node = NULL;
+    vsi_nn_tensor_attr_t attr;
+    vsi_nn_tensor_t * output_tensor[2] = {NULL};
+    float output_scale = vsi_nn_get_tensor_scale(outputs[0]);
+
+    if (vsi_nn_is_broadcast_operaton(inputs, input_num, outputs[0]))
+    {
+        return NULL;
+    }
+
+    memcpy( &attr, &outputs[0]->attr, sizeof(vsi_nn_tensor_attr_t) );
+    attr.dtype.qnt_type = VSI_NN_QNT_TYPE_NONE;
+    attr.dtype.vx_type = VSI_NN_TYPE_FLOAT32;
+    attr.is_const = FALSE;
+    attr.vtl = TRUE;
+    output_tensor[0] = vsi_nn_CreateTensor( graph, &attr );
+    CHECK_PTR_FAIL_GOTO( output_tensor[0], "Create tensor fail.", final );
+    output_tensor[1] = vsi_nn_CreateTensor( graph, &attr );
+    CHECK_PTR_FAIL_GOTO( output_tensor[1], "Create tensor fail.", final );
+
+    node = vsi_nn_sp_select_a_node(graph, inputs[1], inputs[0], output_tensor[0], output_scale);
+    CHECK_PTR_FAIL_GOTO( node, "Create select_a node fail.", final );
+    node = vsi_nn_sp_select_b_node(graph, inputs[2], inputs[0], output_tensor[1], output_scale);
+    CHECK_PTR_FAIL_GOTO( node, "Create select_b node fail.", final );
+    node = vsi_nn_sp_select_add_node(graph, output_tensor[0], output_tensor[1], outputs[0]);
+    CHECK_PTR_FAIL_GOTO( node, "Create select_add node fail.", final );
+
+final:
+    vsi_safe_release_tensor(output_tensor[0]);
+    vsi_safe_release_tensor(output_tensor[1]);
+
+    return node;
+} /* select() */
 
 __END_DECLS
 
