e1ba2676af177b3b68a37f8bf5470d37f78d7056
Haoxiang.Wang <Haoxiang.Wang@verisilicon.com>
2022-11-25 14:23:51 +0800

    refine evis path of matrixmul
    add U8I16toI16 type
    
    Type: Code Improvement
    Change-Id: I031b09fec0b16314fcd0293f39dd04fe94ee0afd

--- ovxlib/src/ops/vsi_nn_op_matrixmul.c
+++ ovxlib/src/ops/vsi_nn_op_matrixmul.c
@@ -150,6 +150,7 @@ static vsi_bool op_check
         IO_TYPE(D_U8|Q_ASYM,  D_U8|Q_ASYM,  D_F16)
         IO_TYPE(D_U8|Q_ASYM,  D_F16,  D_U8|Q_ASYM)
         IO_TYPE(D_U8|Q_ASYM,  D_F16,  D_F16)
+        IO_TYPE(D_U8|Q_ASYM,  D_I16|Q_DFP,  D_I16|Q_DFP)
         IO_TYPE(D_I8|Q_DFP,  D_I8|Q_DFP,  D_I8|Q_DFP)
         IO_TYPE(D_I8|Q_DFP,  D_I8|Q_DFP,  D_F16)
         IO_TYPE(D_I8|Q_DFP,  D_F16,  D_I8|Q_DFP)
