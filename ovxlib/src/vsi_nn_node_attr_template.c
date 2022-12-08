572de983f8b1391b1eaf1b98ae58ed4242f09cca
Haoxiang.Wang <Haoxiang.Wang@verisilicon.com>
2022-12-08 15:15:58 +0800

    Add ReverseSequence operation into ovxlib
    For each slice i iterating on batch axis, the operator reverses
    the first sequence_lens[i] elements on time axis, and copies elements
    whose index's beyond sequence_lens[i] to the output. So the output
    slice i contains reversed sequences on the first sequence_lens[i]
    elements, then have original values copied for the other elements.
    
    Type: New Feature
    Issue:ACUITY-1175
    Change-Id: I73a00405768f06ef98cfd0b51934d33a63880be3

--- ovxlib/src/vsi_nn_node_attr_template.c
+++ ovxlib/src/vsi_nn_node_attr_template.c
@@ -203,6 +203,7 @@ static _node_template s_template[] =
     /* GLOBALLPPOOL */          NULL,
     /* AVG_POOL3D */            NULL,
     /* MAXUNPOOL */             NULL,
+    /* REVERSESEQUENCE */       NULL,
 };
 //_compiler_assert( _cnt_of_array(s_template) == VSI_NN_OP_NUM, vsi_nn_node_attr_template_c );
 
