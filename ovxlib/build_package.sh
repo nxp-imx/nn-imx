eaea989b2678e72254caf1603eb501c046d53654
Jin Yang <jin.yang@verisilicon.com>
2023-02-16 14:08:33 +0800

    Add build_package.sh for ovxlib
    build_package.sh is copied from Acuity commit
    e7003466568147dc12cae2d9645cb2572b24cd18, and
    made some trival change to let it can run in
    ovxlib root folder.
    
    SCM should use ovxlib/build_package.sh to pack
    ovxlib package if it is existed, and then use
    acuity/build_acuity_ovxlib_linux.sh to pack if
    ovxlib/build_package.sh is not existed.
    
    Change-Id: If6cf3cdeab51f45944619edcd79fdba0d94ed3c1
    Type: Code Improvement

---
+++ ovxlib/build_package.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BUILDTYPE="dev"
+DELIVERABLE=./ovxlib-package-$BUILDTYPE
+
+# Package final deliverable
+package_deliverable()
+{
+    # Wipe existing devlierable
+    echo "clean deliverable ..."
+    rm -rf $DELIVERABLE;
+
+    echo "packaging deliverable ..."
+
+    # Copy souce code of ovxlib
+    if [ "$BUILDTYPE" = "dev" ] || [ "$BUILDTYPE" = "rel" ]; then
+        mkdir $DELIVERABLE
+        for item in ./*
+            do
+                if [ "$item" != "$DELIVERABLE" ]; then
+                    echo "copy $item to $DELIVERABLE"
+                    cp $item $DELIVERABLE -rf
+                else
+                    echo "Ignore $item"
+                fi
+            done
+    else
+        exit $?
+    fi
+
+    # remove redundant folders and files
+    rm -f $DELIVERABLE/ovxlib.vcxproj
+    rm -f $DELIVERABLE/ovxlib.vcxproj.filters
+    rm -f $DELIVERABLE/ovxlib.vcxproj.user
+    rm -rf $DELIVERABLE/.git
+    rm -rf $DELIVERABLE/test/internal
+    rm -rf $DELIVERABLE/third-party/gperftools
+    rm -rf $DELIVERABLE/tools/rockchips_toybrick_compiler
+
+    # Tar
+    PKG_FILE=$DELIVERABLE'-'`date +%m%d%Y`.tar.gz
+    rm -rf $PKG_FILE
+    tar -zcf $PKG_FILE $DELIVERABLE
+    echo "Build package completed!"
+}
+
+if [ $# -ne 0 ]; then
+    BUILDTYPE=$1
+fi
+
+if [ "$BUILDTYPE" = "dev" ]; then
+    echo "Development Source Package Build Starting ..."
+else
+    echo "./build_package.sh [dev]"
+    exit $?
+fi
+
+
+# Package
+package_deliverable
+
