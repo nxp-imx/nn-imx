d7556c1b6e17e4e660cdeaa7e51e7939c78d1ed4
Xia Kaihong <kaihong.xia@verisilicon.com>
2023-05-11 10:54:54 +0800

    Optimize expand_broadcast operation
    Convert to dataconvert operation when shape is same
    
    Change-Id: I852cd88cae1a1cc9225284467ff28867e3e53d81
    Type: Code Improvement

--- ovxlib/src/ops/vsi_nn_op_expand_broadcast.c
+++ ovxlib/src/ops/vsi_nn_op_expand_broadcast.c
@@ -113,9 +113,11 @@ static vsi_bool op_setup
     vsi_nn_tensor_attr_t attr;
     vsi_nn_internal_tensor_t* input_0 = NULL;
     vsi_nn_internal_tensor_t *input_1 = NULL;
+    vsi_nn_internal_tensor_t* input_2 = NULL;
     vsi_nn_internal_node_t* mul_node = NULL;
     vsi_nn_tensor_t* mul_input = NULL;
     int32_t use_virtual_tensor = 1;
+    vsi_bool is_same_shape = TRUE;
     vsi_nn_expand_broadcast_param *p = &self->nn_param.expand_broadcast;
 
     vsi_nn_internal_init_node_wksp(self);
@@ -140,7 +142,15 @@ static vsi_bool op_setup
     attr.is_const = TRUE;
     for (i = 0; i < p->dim_num; i++)
     {
+        vsi_size_t sz = i < inputs[0]->attr.dim_num ?
+            inputs[0]->attr.size[i] : 1;
+
         attr.size[i] = p->shape[i];
+        if (( p->shape[i] != sz && p->shape[i] != 1)
+            && is_same_shape)
+        {
+            is_same_shape = FALSE;
+        }
     }
     input_1 = vsi_nn_internal_new_tensor( self, &attr, 1.0f );
     CHECK_PTR_FAIL_GOTO(input_1, "Create tensor failed", final);
@@ -180,15 +190,64 @@ static vsi_bool op_setup
         mul_input = inputs[0];
     }
 
-    mul_node = vsi_nn_internal_new_node(self, VSI_NN_OP_MULTIPLY, 0, 0 );
-    CHECK_PTR_FAIL_GOTO(mul_node, "Create internal node failed", final);
-    mul_node->inputs[0] = mul_input;
-    mul_node->inputs[1] = input_1->t;
-    mul_node->node->nn_param.multiply.scale = 1.0f;
-    mul_node->node->vx_param.overflow_policy = VX_CONVERT_POLICY_SATURATE;
-    mul_node->node->vx_param.rounding_policy = VX_ROUND_POLICY_TO_NEAREST_EVEN;
-    mul_node->outputs[0] = outputs[0];
-    vsi_nn_internal_setup_node(self, mul_node);
+    if (is_same_shape)
+    {
+        vsi_nn_internal_node_t* curr = NULL;
+        vsi_nn_tensor_t* temp_tensor = NULL;
+
+        if (input_1->t->attr.dim_num != mul_input->attr.dim_num)
+        {
+            vsi_size_t* shape_sizes = NULL;
+            uint32_t rank0 = input_1->t->attr.dim_num;
+            uint32_t rank1 = mul_input->attr.dim_num;
+            uint32_t rank = vsi_nn_max( rank0, rank1 );
+
+            memset(&attr, 0, sizeof(vsi_nn_tensor_attr_t));
+            vsi_nn_internal_init_tensor_attr(&attr, &mul_input->attr.dtype, use_virtual_tensor);
+            input_2 = vsi_nn_internal_new_tensor( self, &attr, 0.0f );
+            CHECK_PTR_FAIL_GOTO(input_2, "Create internal tensor failed", final);
+
+            curr = vsi_nn_internal_new_node(self, VSI_NN_OP_RESHAPE2, 0, 0 );
+            CHECK_PTR_FAIL_GOTO(curr, "Create internal node failed", final);
+            shape_sizes = (vsi_size_t*)vsi_nn_internal_new_node_param(curr,
+                VSI_NN_MAX_DIM_NUM * sizeof(vsi_size_t));
+            CHECK_PTR_FAIL_GOTO_RLS_INTERNAL_NODE(shape_sizes, curr,
+                "Create internal buffer failed", final);
+            for (i = 0; i < rank; i++)
+            {
+                shape_sizes[i] = i < rank1 ? mul_input->attr.size[i] : 1;
+            }
+            curr->node->nn_param.reshape2.size = shape_sizes;
+            curr->node->nn_param.reshape2.dim_num = rank;
+            curr->inputs[0] = mul_input;
+            curr->outputs[0] = input_2->t;
+            vsi_nn_internal_setup_node( self, curr );
+
+            temp_tensor = input_2->t;
+        }
+        else
+        {
+            temp_tensor = mul_input;
+        }
+
+        curr = vsi_nn_internal_new_node(self, VSI_NN_OP_DATACONVERT, 0, 0 );
+        CHECK_PTR_FAIL_GOTO(curr, "Create internal node failed", final);
+        curr->inputs[0] = temp_tensor;
+        curr->outputs[0] = outputs[0];
+        vsi_nn_internal_setup_node(self, curr);
+    }
+    else
+    {
+        mul_node = vsi_nn_internal_new_node(self, VSI_NN_OP_MULTIPLY, 0, 0 );
+        CHECK_PTR_FAIL_GOTO(mul_node, "Create internal node failed", final);
+        mul_node->inputs[0] = mul_input;
+        mul_node->inputs[1] = input_1->t;
+        mul_node->node->nn_param.multiply.scale = 1.0f;
+        mul_node->node->vx_param.overflow_policy = VX_CONVERT_POLICY_SATURATE;
+        mul_node->node->vx_param.rounding_policy = VX_ROUND_POLICY_TO_NEAREST_EVEN;
+        mul_node->outputs[0] = outputs[0];
+        vsi_nn_internal_setup_node(self, mul_node);
+    }
 
     return TRUE;
 final:
