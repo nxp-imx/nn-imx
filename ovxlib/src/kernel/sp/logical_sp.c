2fdb474b6c937f66466a4468f4d9b9074c3a3364
Zhouheng.Zheng <Zhouheng.Zheng@verisilicon.com>
2023-05-31 16:29:31 +0800

    Support logical related vx node
    1.Remove logical related sp implement
    2.Replace it by vxLogicalNotLayer and vxLogicalOpsLayer node
    
    Change-Id: I0db960e31388e153df085cdf9f592ddaa06dc0cb
    Type: Code Improvement
    Issue: VIVD-668

--- ovxlib/src/kernel/sp/logical_sp.c
+++ ovxlib/src/kernel/sp/logical_sp.c
@@ -34,311 +34,8 @@
 #include "kernel/vsi_nn_sp_unit_operation.h"
 #include "kernel/vsi_nn_sp_lut.h"
 
-#if (VX_STREAM_PROCESSOR_SUPPORT)
+#if defined(VX_STREAM_PROCESSOR_SUPPORT) && defined(VX_LOGICAL_VX_SUPPORT)
 __BEGIN_DECLS
-
-vsi_nn_kernel_node_t vsi_nn_sp_logical_and_node
-    (
-        vsi_nn_graph_t              * graph,
-        vsi_nn_tensor_t             * input0,
-        vsi_nn_tensor_t             * input1,
-        vsi_nn_tensor_t             * output,
-        char                        * kernel_name
-    )
-{
-    const int32_t spInitInstsNum = 0;
-    const int32_t spLoopInstsNum = 2;
-    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
-
-    const uint32_t input_count = 2;
-    const uint32_t output_count = 1;
-    vx_tensor inputs_tensor[2] = {NULL};
-    vx_tensor outputs_tensor[1] = {NULL};
-    vx_node node = NULL;
-
-    vsi_nn_spinst_t *spinst = NULL;
-    vsi_nn_spinst_inst_param sp_insts_param[2];
-    vsi_nn_spinst_attr_t attr;
-
-    vsi_status status = VSI_FAILURE;
-
-    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
-    vsi_nn_init_spinst_attr(&attr);
-
-    /* loop inst0: out = r1 * r2 | r1 = in */
-    status  = vsi_nn_sp_mul(&sp_insts_param[0], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SROUT);
-    status |= vsi_nn_sp_move(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR1);
-    /* loop inst1: r2 = in */
-    status |= vsi_nn_sp_move(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR2);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    attr.ignored_leading_outputs = 2;
-    attr.flush_cycle_num = 3;
-
-    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
-    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
-
-    attr.prog_init_instr_num = spInitInstsNum;
-    attr.prog_loop_instr_num = spLoopInstsNum;
-
-    spinst = vsi_nn_create_spinst(graph);
-    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
-    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
-    status |= vsi_nn_set_spinst_attr(spinst, attr);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    inputs_tensor[0] = input0->t;
-    inputs_tensor[1] = input1->t;
-    outputs_tensor[0] = output->t;
-    node = vxStreamProcessorNode(
-        graph->g,
-        inputs_tensor,
-        input_count,
-        outputs_tensor,
-        output_count,
-        spinst->sp,
-        NULL);
-
-    status = vsi_nn_set_sp_kernel_name(node, kernel_name);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-final:
-    if (spinst)
-    {
-        vsi_nn_release_spinst(&spinst);
-    }
-
-    return (vsi_nn_kernel_node_t)node;
-}
-
-vsi_nn_kernel_node_t vsi_nn_sp_logical_or_node
-    (
-        vsi_nn_graph_t              * graph,
-        vsi_nn_tensor_t             * input0,
-        vsi_nn_tensor_t             * input1,
-        vsi_nn_tensor_t             * output,
-        char                        * kernel_name
-    )
-{
-    const int32_t spInitInstsNum = 0;
-    const int32_t spLoopInstsNum = 2;
-    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
-
-    const uint32_t input_count = 2;
-    const uint32_t output_count = 1;
-    vx_tensor inputs_tensor[2] = {NULL};
-    vx_tensor outputs_tensor[1] = {NULL};
-    vx_node node = NULL;
-
-    vsi_nn_spinst_t *spinst = NULL;
-    vsi_nn_spinst_inst_param sp_insts_param[2];
-    vsi_nn_spinst_attr_t attr;
-
-    vsi_status status = VSI_FAILURE;
-
-    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
-    vsi_nn_init_spinst_attr(&attr);
-
-    /* loop inst0: r6 = r1 + r2 | r1 = in */
-    status  = vsi_nn_sp_add(&sp_insts_param[0], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SR6);
-    status |= vsi_nn_sp_move(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR1);
-    /* loop inst1: r2 = in * r3 | out = r6 ? r3 : r4 */
-    status |= vsi_nn_sp_mul(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR2);
-    status |= vsi_nn_sp_move_sel0(&sp_insts_param[1], VSI_NN_SP_SR6, VSI_NN_SP_SR3, VSI_NN_SP_SROUT);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    attr.ignored_leading_outputs = 3;
-    attr.flush_cycle_num = 6;
-
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, 1);
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR4(attr, 0);
-
-    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
-    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
-
-    attr.prog_init_instr_num = spInitInstsNum;
-    attr.prog_loop_instr_num = spLoopInstsNum;
-
-    spinst = vsi_nn_create_spinst(graph);
-    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
-    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
-    status |= vsi_nn_set_spinst_attr(spinst, attr);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    inputs_tensor[0] = input0->t;
-    inputs_tensor[1] = input1->t;
-    outputs_tensor[0] = output->t;
-    node = vxStreamProcessorNode(
-        graph->g,
-        inputs_tensor,
-        input_count,
-        outputs_tensor,
-        output_count,
-        spinst->sp,
-        NULL);
-
-    status = vsi_nn_set_sp_kernel_name(node, kernel_name);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-final:
-    if (spinst)
-    {
-        vsi_nn_release_spinst(&spinst);
-    }
-
-    return (vsi_nn_kernel_node_t)node;
-}
-
-vsi_nn_kernel_node_t vsi_nn_sp_logical_xor_node
-    (
-        vsi_nn_graph_t              * graph,
-        vsi_nn_tensor_t             * input0,
-        vsi_nn_tensor_t             * input1,
-        vsi_nn_tensor_t             * output,
-        char                        * kernel_name
-    )
-{
-    const int32_t spInitInstsNum = 0;
-    const int32_t spLoopInstsNum = 2;
-    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
-
-    const uint32_t input_count = 2;
-    const uint32_t output_count = 1;
-    vx_tensor inputs_tensor[2] = {NULL};
-    vx_tensor outputs_tensor[1] = {NULL};
-    vx_node node = NULL;
-
-    vsi_nn_spinst_t *spinst = NULL;
-    vsi_nn_spinst_inst_param sp_insts_param[2];
-    vsi_nn_spinst_attr_t attr;
-
-    vsi_status status = VSI_FAILURE;
-
-    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
-    vsi_nn_init_spinst_attr(&attr);
-
-    /* loop inst0: r1 = in * r3 | r6 = r1 - r2 | out = r5 ? r3 : r4 */
-    status  = vsi_nn_sp_mul(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SR1);
-    status |= vsi_nn_sp_sub(&sp_insts_param[0], VSI_NN_SP_SR1, VSI_NN_SP_SR2, VSI_NN_SP_SR6);
-    status |= vsi_nn_sp_move_sel0(&sp_insts_param[0], VSI_NN_SP_SR5, VSI_NN_SP_SR3, VSI_NN_SP_SROUT);
-    /* loop inst1: r2 = in + r4 | r5 = abs(r6) */
-    status |= vsi_nn_sp_add(&sp_insts_param[1], VSI_NN_SP_SRIN, VSI_NN_SP_SR4, VSI_NN_SP_SR2);
-    status |= vsi_nn_sp_abs(&sp_insts_param[1], VSI_NN_SP_SR6, VSI_NN_SP_SR5);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    attr.ignored_leading_outputs = 5;
-    attr.flush_cycle_num = 9;
-
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, 1);
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR4(attr, 0);
-
-    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
-    attr.input_setup = VSI_NN_SP_INPUT_SETUP_INTERLEAVE_TWO_INPUT;
-
-    attr.prog_init_instr_num = spInitInstsNum;
-    attr.prog_loop_instr_num = spLoopInstsNum;
-
-    spinst = vsi_nn_create_spinst(graph);
-    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
-    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
-    status |= vsi_nn_set_spinst_attr(spinst, attr);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    inputs_tensor[0] = input0->t;
-    inputs_tensor[1] = input1->t;
-    outputs_tensor[0] = output->t;
-    node = vxStreamProcessorNode(
-        graph->g,
-        inputs_tensor,
-        input_count,
-        outputs_tensor,
-        output_count,
-        spinst->sp,
-        NULL);
-
-    status = vsi_nn_set_sp_kernel_name(node, kernel_name);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-final:
-    if (spinst)
-    {
-        vsi_nn_release_spinst(&spinst);
-    }
-
-    return (vsi_nn_kernel_node_t)node;
-}
-
-vsi_nn_kernel_node_t vsi_nn_sp_logical_not_node
-    (
-        vsi_nn_graph_t              * graph,
-        vsi_nn_tensor_t             * input,
-        vsi_nn_tensor_t             * output,
-        char                        * kernel_name
-    )
-{
-    const int32_t spInitInstsNum = 0;
-    const int32_t spLoopInstsNum = 1;
-    const int32_t spInstsNum = spInitInstsNum + spLoopInstsNum;
-
-    const uint32_t input_count = 1;
-    const uint32_t output_count = 1;
-    vx_tensor inputs_tensor[1] = {NULL};
-    vx_tensor outputs_tensor[1] = {NULL};
-    vx_node node = NULL;
-
-    vsi_nn_spinst_t *spinst = NULL;
-    vsi_nn_spinst_inst_param sp_insts_param[1];
-    vsi_nn_spinst_attr_t attr;
-
-    vsi_status status = VSI_FAILURE;
-
-    memset(sp_insts_param, 0, sizeof(vsi_nn_spinst_inst_param) * spInstsNum);
-    vsi_nn_init_spinst_attr(&attr);
-
-    /* loop inst0: out = in ? r3 : r4 */
-    status = vsi_nn_sp_move_sel0(&sp_insts_param[0], VSI_NN_SP_SRIN, VSI_NN_SP_SR3, VSI_NN_SP_SROUT);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    attr.ignored_leading_outputs = 0;
-    attr.flush_cycle_num = 0;
-
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR3(attr, 0);
-    VSI_NN_SP_ATTR_SET_CONST_TO_SR4(attr, 1);
-
-    attr.input_tile_mapping = VSI_NN_SP_ATTR_INPUT_TILE_MAPPING_XYMERGE;
-    attr.input_setup = VSI_NN_SP_INPUT_SETUP_SINGLE_INPUT;
-
-    attr.prog_init_instr_num = spInitInstsNum;
-    attr.prog_loop_instr_num = spLoopInstsNum;
-
-    spinst = vsi_nn_create_spinst(graph);
-    CHECK_PTR_FAIL_GOTO( spinst, "Create spInst fail.", final );
-    status  = vsi_nn_add_spinst_insts(spinst, sp_insts_param, spInstsNum);
-    status |= vsi_nn_set_spinst_attr(spinst, attr);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-    inputs_tensor[0] = input->t;
-    outputs_tensor[0] = output->t;
-    node = vxStreamProcessorNode(
-        graph->g,
-        inputs_tensor,
-        input_count,
-        outputs_tensor,
-        output_count,
-        spinst->sp,
-        NULL);
-
-    status = vsi_nn_set_sp_kernel_name(node, kernel_name);
-    CHECK_STATUS_FAIL_GOTO(status, final );
-
-final:
-    if (spinst)
-    {
-        vsi_nn_release_spinst(&spinst);
-    }
-
-    return (vsi_nn_kernel_node_t)node;
-}
 
 #define REGISTER_LOGICAL_STREAM_PROCESSOR_KERNEL( kernel_name )   \
     static vsi_nn_kernel_node_t _##kernel_name##setup \
@@ -365,52 +62,46 @@ final:
 
 REGISTER_LOGICAL_STREAM_PROCESSOR_KERNEL( logical_ops )
 {
-    int32_t ops_type  = vsi_nn_kernel_param_get_int32( params, "ops_type" );
     vsi_nn_kernel_node_t node = NULL;
+    int32_t ops_type  = vsi_nn_kernel_param_get_int32( params, "ops_type" );
+    vx_tensor inputs_tensor[2] = {NULL};
+    vx_tensor output_tensor = NULL;
 
-    VSI_UNREFERENCED(input_num);
-    VSI_UNREFERENCED(output_num);
+    inputs_tensor[0] = inputs[0]->t;
+    inputs_tensor[1] = inputs[1]->t;
+    output_tensor = outputs[0]->t;
+
     VSI_UNREFERENCED(kernel);
+    VSI_UNREFERENCED(output_num);
 
-    if ( vsi_nn_is_broadcast_operaton(inputs, input_num, outputs[0]) )
-    {
-        return NULL;
-    }
-
-    switch ( ops_type )
-    {
-    case VSI_NN_LOGICAL_AND:
-        node = vsi_nn_sp_logical_and_node(graph, inputs[0], inputs[1], outputs[0], "logical_and");
-        break;
-    case VSI_NN_LOGICAL_OR:
-        node = vsi_nn_sp_logical_or_node(graph, inputs[0], inputs[1], outputs[0], "logical_or");
-        break;
-    case VSI_NN_LOGICAL_XOR:
-        node = vsi_nn_sp_logical_xor_node(graph, inputs[0], inputs[1], outputs[0], "logical_xor");
-        break;
-    default:
-        return NULL;
-    }
-
-    CHECK_PTR_FAIL_GOTO( node, "Create sp_logical node fail.", final );
-
+    node = vxLogicalOpsLayer(
+        graph->g,
+        ops_type,
+        inputs_tensor,
+        (vx_uint32)input_num,
+        output_tensor
+        );
+    CHECK_PTR_FAIL_GOTO( node, "Create logical ops node  fail.", final );
 final:
-
-    return node;
+    return (vsi_nn_kernel_node_t)node;
 } /* logical_ops() */
 
 REGISTER_LOGICAL_STREAM_PROCESSOR_KERNEL( logical_not )
 {
     vsi_nn_kernel_node_t node = NULL;
 
-    VSI_UNREFERENCED(input_num);
-    VSI_UNREFERENCED(output_num);
     VSI_UNREFERENCED(kernel);
+    VSI_UNREFERENCED(output_num);
+    VSI_UNREFERENCED(input_num);
     VSI_UNREFERENCED(params);
-
-    node = vsi_nn_sp_logical_not_node(graph, inputs[0], outputs[0], "logical_not");
-
-    return node;
+    node = vxLogicalNotLayer(
+        graph->g,
+        inputs[0]->t,
+        outputs[0]->t
+        );
+    CHECK_PTR_FAIL_GOTO( node, "Create logical not node  fail.", final );
+final:
+    return (vsi_nn_kernel_node_t)node;
 } /* logical_not() */
 
 
